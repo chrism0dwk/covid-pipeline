5d3650d95f6ede3aab840b49e0e1a30b4cb97459
Chris Jewell <chrism0dwk@gmail.com>
2021-02-09 22:21:03 +0000

    Implemented  in shell scripts

--- enqueue_pipeline.sh
+++ enqueue_pipeline.sh
@@ -10,60 +10,49 @@
 # Usage: enqueue_pipeline.sh <ISO8601 date>	    #
 #####################################################
 
+
+
+while getopts 'c:d:s:r:' OPTION; do
+    case "$OPTION" in
+	c)
+	    arg_config=${OPTARG}
+	    ;;
+	d)
+	    arg_date=${OPTARG}
+	    ;;
+	s)
+	    arg_staging_dir=${OPTARG}
+	    ;;
+	r)
+	    arg_results_dir=${OPTARG}
+	    ;;
+	:)
+	    exit 1
+	    ;;
+	*)
+	    echo "usage: enqueue_pipeline.sh -c <config file> -d <ISO6801 date> -s <staging dir> -r <results_dir>"
+	    exit 1
+	    ;;
+    esac
+done
+
+
 # Fix for Lancaster HEC environment
 source /usr/shared_apps/admin/etc/sge/switch-gpu.sh
 
 # Environment variables for date and results staging
-export COVIDREFDATE=$@
-export COVIDRESULTSDIR=${global_scratch}/covid_pipeline/$COVIDREFDATE
-JOBNAME=c19_$COVIDREFDATE
-STDOUT=$COVIDRESULTSDIR/stdout.txt
+JOBNAME=c19_$arg_date_$arg_config
+STDOUT=$arg_staging_dir/stdout.txt
 
 # Final results dir
 RESULTS_DEST_DIR=/mnt/covid/c19/covid_pipeline
 
-# Email success/fail
-MAILTO="c.jewell@lancaster.ac.uk"
+mkdir -p ${arg_staging_dir}
+qsub -N $JOBNAME -o $STDOUT \
+     pipeline.sge \
+     -c "$arg_config" \
+     -d "$arg_date" \
+     -s "$arg_staging_dir" \
+     -r "$arg_results_dir"
 
-
-
-stdout_str() {
-    if [ -f "$STDOUT" ]
-    then
-	STDOUT_CONTENT=$(<$STDOUT)
-    fi
-    cat <<EOF 
-===STDOUT===
-$STDOUT_CONTENT
-===STDOUT===
-EOF
-}    
-
-
-
-email_results() {
-    stdout_str | mail -s "[COVID Pipeline] ${1:-"Unknown"}" $MAILTO
-}
-
-
-
-error_exit() {
-    MSG=${1:-"Unknown Error"}
-    email_results "Error $MSG"
-    echo $MSG 1>&2
-    exit 1
-}
-
-mkdir -p $COVIDRESULTSDIR ||
-    error_exit "Cannot create output directory $COVIDRESULTSDIR"
-
-qsub -sync y -N c19_$COVIDREFDATE -o $STDOUT \
-     -v COVIDREFDATE="$COVIDREFDATE" -v COVIDRESULTSDIR="$COVIDRESULTSDIR" \
-     pipeline.sge >>$STDOUT 2>&1 || error_exit "Pipeline job failed"
-
-rsync -a ${COVIDRESULTSDIR} ${RESULTS_DEST_DIR} >>$STDOUT 2>&1 ||
-    error_exit "Error copying $COVIDRESULTSDIR to $RESULTS_DEST_DIR"
-
-email_results "Success"
 exit 0
-
